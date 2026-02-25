07b394fdc076a40ecf7c5c5d48a558fa7e22f080
RonaldSBz <ronaldsbsantosdebrito@gmail.com>
2026-02-25 13:32:54 -0300

    correção da logo funkybox, nome grup1.svg

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,7 +8,7 @@ const RootLayout = () => {
     <GlobalStyles>
       <Stack>
         <Stack.Protected guard={isLoggedIn}>
-          <Stack.Screen options={{ headerShown: false }} name="(tab)" />
+          <Stack.Screen options={{ headerShown: false }} name="introducao" />
         </Stack.Protected>
         <Stack.Protected guard={!isLoggedIn}>
           <Stack.Screen options={{ headerShown: false }} name="login" />
